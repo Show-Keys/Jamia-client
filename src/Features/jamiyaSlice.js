f3ceb5f72729f6620571a8cdabd8a2d799b9d8f6
Show-Key <127041780+Show-Keys@users.noreply.github.com>
2025-01-28 22:47:41 +0400

    dsadfasfd

--- src/Features/jamiyaSlice.js
+++ src/Features/jamiyaSlice.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create a standalone action
 export const clearMessage = createAction("jamiyas/clearMessage");
-
+//abu saif
 export const saveJamiya = createAsyncThunk("jamiyas/saveJamiya", async (jamiyaData, thunkAPI) => {
   try {
     const response = await axios.post("http://127.0.0.1:5000/api/jamiya/saveJamiya", {
